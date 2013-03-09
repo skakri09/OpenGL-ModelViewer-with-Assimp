5fda0fcd390abdc0b575ef0e5be31a25cf356739
Kristian Skarseth <kristianskarseth@msn.com>
2013-03-09 11:09:25 +0100

    Did the last change. The threaded functions should be working now. Lacking debugging though.

--- Innlevering1/src/VideoThreadedUtil.cpp
+++ Innlevering1/src/VideoThreadedUtil.cpp
@@ -33,19 +33,20 @@ void WriteFramesToDisk( vfb_ptr video_frame_buffer, vwm_ptr video_writer, bool f
 {
 	//placeholder for compiling. Change thread to actual thread:
 	boost::thread::id thread_id;
+
 	video_writer_ptr vid_writer = video_writer->Lock_GetVideoWriter(thread_id);
 	vf_deque_ptr vf_dq_ptr = video_frame_buffer->Lock_GetVideoFrameBuffer(thread_id);
 
-	for(unsigned int i = 0; i < video_frame_buffer->video_frames_buffer->size(); i++)
+	for(unsigned int i = 0; i < vf_dq_ptr->size(); i++)
 	{
-		vf_ptr p = video_frame_buffer->video_frames_buffer->at(i);
+		vf_ptr p = vf_dq_ptr->at(i);
 
 		if(flip)
 			p->Flip();
 
-		*video_writer->video_writer << *p->image;
+		*vid_writer << *p->image;
 	}
-
-	video_writer->vw_mutex.unlock();
-	video_frame_buffer->vfb_mutex.unlock();
+	
+	video_writer->ReleaseMutex(thread_id);
+	video_frame_buffer->ReleaseMutex(thread_id);
 }
